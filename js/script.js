c7b3e2816a24053b5daecf7b3e22bfd6e5195974
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-29 20:38:59 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-// Copyright (c) 2020 Janet Do All rights reserved
-//
 // Created by: Janet Do
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
-function myButtonClicked() {
-  alwaysOnButtonChecked = document.getElementById("on-check").checked
 
-  if (alwaysOnButtonChecked == true) {
-    document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: On</p>"
+function myButtonClicked() {
+  const positiveButtonChecked =
+    document.getElementById("positive-check").checked
+
+  let randomNumber
+  if (positiveButtonChecked) {
+    // Generate a random positive number
+    randomNumber = Math.floor(Math.random() * 6) + 1
   } else {
-    document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: Off</p>"
+    // Generate a random negative number
+    randomNumber = -1 * (Math.floor(Math.random() * 6) + 1)
   }
+
+  document.getElementById("random-number-value").innerHTML =
+    "<p>Random Number is: " + randomNumber + "</p>"
 }
