5c45cb8087464969296347b96907b97972672f87
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-29 20:18:33 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById("on-check").checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: On</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: Off</p>"
+  }
+}
